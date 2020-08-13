be91e3f4ceb36e7a892f4f397924c72cdf69fe66
rdhlb <maxradohleb@gmail.com>
2020-08-13 12:28:55 +0300

    add gitignore and sample code

--- app/index.js
+++ app/index.js
@@ -1,1 +1,27 @@
-console.log('Hello world!');
+import document from "document";
+import clock from "clock";
+
+let myClock = document.getElementById("myClock");
+
+clock.granularity = 'seconds'; // seconds, minutes, hours
+
+clock.ontick = function(evt) {
+  myClock.text = ("0" + evt.date.getHours()).slice(-2) + ":" +
+                      ("0" + evt.date.getMinutes()).slice(-2) + ":" +
+                      ("0" + evt.date.getSeconds()).slice(-2);
+};
+
+let demo = document.getElementById("demo");
+// demo.style.display = "none";
+// demo.style.display = "inline";
+
+// Toggle Show/Hide
+function toggle(ele) {
+  ele.style.display = (ele.style.display === "inline") ? "none" : "inline";
+}
+
+toggle(demo);
+
+setTimeout(() => {
+  toggle(demo);
+}, 1000);
