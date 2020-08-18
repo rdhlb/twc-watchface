e3af4f56fbb84e04ca503335f00f31fd6eaca10c
rdhlb <maxradohleb@gmail.com>
2020-08-18 19:52:28 +0300

    add some more logging

--- app/index.ts
+++ app/index.ts
@@ -12,15 +12,19 @@ import { processWeatherData } from './weather';
 // TODO: rewrite to just periodically send weather/calendar data from companion app?
 
 const errorNode = document.getElementById('error');
-const closeNode = document.getElementById('error');
+const openNode = document.getElementById('open');
+const closeNode = document.getElementById('close');
 const lastMessageNode = document.getElementById('lastMessage');
 const lastSyncTimeNode = document.getElementById('lastSyncTime');
-const startWeatherPolling = () => setInterval(fetchWeather, 0.5 * 1000 * 60); // 5 minutes
+const startWeatherPolling = () => setInterval(fetchWeather, 5 * 1000 * 60); // 5 minutes
 let lastTime = Date.now();
 
 function fetchWeather() {
-  lastMessageNode.text = `fetchWeather invoked. socket: ${messaging.peerSocket.bufferedAmount}, ${messaging.peerSocket.readyState}`;
-  lastSyncTimeNode.text = device.lastSyncTime?.toLocaleTimeString();
+  const time = new Date();
+  lastMessageNode.text = `fetchWeather(). ${messaging.peerSocket.bufferedAmount}, ${
+    messaging.peerSocket.readyState === messaging.peerSocket.OPEN
+  }, ${time.getMinutes()}:${time.getSeconds()}`;
+  lastSyncTimeNode.text = `${device.lastSyncTime?.toString()}`;
 
   if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
     lastMessageNode.text = 'peerSocket in Open state';
@@ -31,13 +35,12 @@ function fetchWeather() {
 }
 
 messaging.peerSocket.onopen = function () {
+  openNode.text = 'onopen';
   fetchWeather();
 };
 
 messaging.peerSocket.onmessage = function (evt) {
   const messageTime = Date.now();
-  console.log(messageTime);
-  console.log(lastTime);
   const minutesSince = (messageTime - lastTime) / 1000 / 60;
   lastTime = messageTime;
   lastMessageNode.text = `Last message received ${minutesSince.toFixed(2)} min ago.`;
